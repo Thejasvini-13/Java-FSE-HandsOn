54144699e7e62d50094ab3ea2c9dcd5d7044ad24
Thejasvini-13 <thejaswinimurugalingam@gmail.com>
2026-06-21 21:21:05 +0530

    Add files via upload

---
+++ Week-1/JUnit-Basic-Testing-Exercises/Exercise-2-Writing Basic JUnit Tests/SourceCode/src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+}
